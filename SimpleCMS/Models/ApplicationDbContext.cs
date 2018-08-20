dc5499d55d166de562601ae6a640ef5ff8ee909c
jingyongly <jingyongly@163.com>
2018-08-20 17:15:58 +0800

    qq

---
+++ SimpleCMS/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+namespace SimpleCMS.Models
+{
+    public class ApplicationDbContext
+    {
+    }
+}
